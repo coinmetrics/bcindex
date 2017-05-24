feb3f2207324d1c429ebd2960fb77efd056f3be4
Muhammad K Khan <mk.muhammad.khan10@gmail.com>
2017-05-23 22:11:49 -0500

    front page is good
    most of the style for the front-end is good, just need to get the button
    to respond and change the chart accordingly

--- index-view/src/main/resources/static/js/app.js
+++ index-view/src/main/resources/static/js/app.js
@@ -1,47 +1,74 @@
+(function () {
+    fetch('/api/index')
+    .then(r => r.text())
+    .then(d => JSON.parse(d))
+    .then(function(result) { 
 
+        let ctx = document.getElementById("myChart");
 
-
-fetch('/api/index')
-.then(r => r.text())
-.then(d => JSON.parse(d))
-.then(function(result) { 
-
-    let ctx = document.getElementById("myChart");
-
-    var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: result.times,
-            datasets: [{
-                label: 'The Index',
-                data: result.data,
-                backgroundColor: 'rgba(255,153,0,0.4)' ,
-                borderColor: 'rgba(255, 159, 64, 1)',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            responsive: true,
-            maintainAspectRatio: true,
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    },
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'Price'
-                    }
-                }],
-                xAxes: [{
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'Time'
-                    }
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: result.times,
+                datasets: [{
+                    label: 'The Index',
+                    data: result.data,
+                    backgroundColor: 'rgba(255,153,0,0.4)' ,
+                    borderColor: 'rgba(255, 159, 64, 1)',
+                    borderWidth: 1
                 }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: true,
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        },
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Price'
+                        }
+                    }],
+                    xAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Time'
+                        }
+                    }]
+                }
             }
-        }
+        });
+    });
+    
+    $('.index').select2({
+        data: [
+            {
+                id: 0,
+                text: 'odd',
+                selected: true
+            },
+            {
+                id: 1,
+                text: 'even'
+            }
+        ],
+        minimumResultsForSearch: Infinity
+    });
+    $('.currency').select2({
+        data: [
+            {
+                id: 0,
+                text: 'usd',
+                selected: true
+            },
+            {
+                id: 1,
+                text: 'bit coin'
+            }
+        ],
+        minimumResultsForSearch: Infinity
     });
 
-
-});    
+})();
