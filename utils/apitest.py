1c7efaccb938c89a719955957e2ded22deb55404
steve fro <fro@uwalumni.com>
2017-06-23 00:39:24 -0500

    more support for db back fill

--- utils/apitest.py
+++ utils/apitest.py
@@ -4,10 +4,12 @@ import datetime
 
 local = "http://localhost:8090/api/index"
 stage = "http://stage-index-view.herokuapp.com/api/index"
+prod = "http://www.bletchleyindexes.com/api/index";
 
-data = {'index':'ODD', 'currency':'USD','timeFrame':'MAX'}
+data = {'index':'ODD', 'currency':'USD','timeFrame':'MONTHLY'}
 
 #resp = requests.post(stage, json=data)
-resp = requests.post(local, json=data)
+#resp = requests.post(local, json=data)
+resp = requests.post(prod, json=data)
 
 print resp.text
--- utils/replay_historical.py
+++ utils/replay_historical.py
@@ -3,8 +3,26 @@ import json
 import datetime
 import csv
 
-def create_file(symbol,mult,even):
+class Mult:
+	val = 0.0
+	even = 0.0
 
+
+# tenMult = Mult()
+# tenMult.val = 8.0
+# print tenMult.val
+
+def get_historical(symbol):
+	url = "http://www.coincap.io/history/30day/" + symbol
+	response = requests.get(url).json()
+
+	if response is not None:
+		return response['price']
+	else:
+		print "no data for " + symbol
+		return response
+
+def create_file(symbol,mult,even):	
 	# get data
 	url = "http://www.coincap.io/history/30day/" + symbol
 	
@@ -34,12 +52,31 @@ def create_line(date_num, price):
 	return str(formatted) + "," + str(price) + str('\n')
 
 #### begin script ####
-# coinfile = open("May_rebal.csv", 'r')
-# for coin in coinfile:
-# 	create_file(coin.rstrip())
 
+# populate data structures
+api = {}
+multData = {}
+numPrices = 0
+cnt = 1
 with open('May_rebal.csv','rb') as csvfile:
 	creader = csv.reader(csvfile, delimiter=',')
 	for row in creader:
-      	create_file(row[0], float(row[1]), float(row[2]))
+		coin = row[0]
+		# api[coin] = get_historical(coin)
+
+		# if numPrices == 0:
+		# 	numPrices = len(api[coin])
+		# 	print "NUM OF PRICES IS: " + str(numPrices)
+
+		# if cnt > 3:
+		# 	break
+
+		tenMult = Mult()
+		tenMult.val = float(row[1])
+		tenMult.even = float(row[2])
+		multData[coin] = tenMult
+
+		print coin 
+		cnt = cnt + 1
+#      	create_file(row[0], float(row[1]), float(row[2]))
  
