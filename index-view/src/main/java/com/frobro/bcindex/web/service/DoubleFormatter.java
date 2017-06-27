d91f36e26a561155884b51b5bfeb0d9303a78bc1
steve fro <fro@uwalumni.com>
2017-06-27 14:31:00 -0500

    #24 add trailing zeros on the server side

--- index-view/src/main/java/com/frobro/bcindex/web/service/DoubleFormatter.java
+++ index-view/src/main/java/com/frobro/bcindex/web/service/DoubleFormatter.java
@@ -6,8 +6,8 @@ import java.text.DecimalFormat;
  * Created by rise on 6/5/17.
  */
 public class DoubleFormatter {
-  private final static DecimalFormat USD_FORMAT = new DecimalFormat("#.##");
-  private final static DecimalFormat BTC_FORMAT = new DecimalFormat("#.###");
+  private final static DecimalFormat USD_FORMAT = new DecimalFormat("#.00");
+  private final static DecimalFormat BTC_FORMAT = new DecimalFormat("#.000");
 
   public static double formatUsd(double raw) {
     return format(raw, USD_FORMAT);
