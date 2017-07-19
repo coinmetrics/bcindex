e9badb817e21bf4e4817143a1e368b943d1f787e
Stoinkler <stoinkler@gmail.com>
2017-07-19 16:19:36 -0500

    july rebalance updates - grab new csvs!

--- index-data/src/main/java/com/frobro/bcindex/web/constants/StaticValues.java
+++ index-data/src/main/java/com/frobro/bcindex/web/constants/StaticValues.java
@@ -5,19 +5,19 @@ package com.frobro.bcindex.web.constants;
  */
 public class StaticValues {
   // 10 index values
-  public static final double DIVISOR_TEN = 222310310.380042;
-  public static final double DIVISOR_EVEN_TEN = 99382603.3786684;
-  public static final double CONSTANT_TEN = 16399912;
-  public static final double CONSTANT_EVEN_TEN = 3575708.58522349;
-  public static final String MKT_CAP_FILE = "business_rules/jun_rebal.csv";
+  public static final double DIVISOR_TEN = 224531788.5;
+  public static final double DIVISOR_EVEN_TEN = 107149793.2;
+  public static final double CONSTANT_TEN = 16453275;
+  public static final double CONSTANT_EVEN_TEN = 3065505.038;
+  public static final String MKT_CAP_FILE = "business_rules/jul_rebal.csv";
 
   // 20 index values
-  public static final double DIVISOR_20 = 8081545.84;
-  public static final double DIVISOR_EVEN_20 = 6708222.46790083;
-  public static final String MKT_CAP_FILE_20 = "business_rules/20_jun_rebal.csv";
+  public static final double DIVISOR_20 = 8843361.673;
+  public static final double DIVISOR_EVEN_20 = 7099807.684;
+  public static final String MKT_CAP_FILE_20 = "business_rules/20_jul_rebal.csv";
 
   // Ethereum index values
   public static final double DIVISOR_ETHER = 20336012.9802558;
   public static final double DIVISOR_EVEN_ETHER = DIVISOR_ETHER;
-  public static final String MKT_CAP_FILE_EHTER = "business_rules/Eth_june.csv";
+  public static final String MKT_CAP_FILE_EHTER = "business_rules/Eth_jul.csv";
 }
