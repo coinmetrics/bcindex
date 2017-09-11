be5c9dbe7592c1e64e56dae80f497801c645f791
steve fro <fro@uwalumni.com>
2017-09-10 21:46:40 -0500

    #37 menu toggle for home page

---
+++ index-view/src/main/resources/static/js/menuService.js
@@ -0,0 +1,8 @@
+function menuToggle(x) {
+    var x = document.getElementById('openMenu');
+    if (x.style.display === 'block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'block';
+    }
+}
