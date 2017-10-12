7ee1c7ce0d3b27773945f7a2bfc5583c77160477
steve fro <fro@uwalumni.com>
2017-10-11 23:46:30 -0500

    point the test to the prod public api

--- utils/apitest.py
+++ utils/apitest.py
@@ -6,14 +6,11 @@ local = "http://localhost:8090/api/index"
 stage = "http://stage-index-view.herokuapp.com/api/index"
 prod = "http://www.bletchleyindexes.com/api/index";
 
-data = {'index':'ODD', 'currency':'USD','timeFrame':'MONTHLY'}
+data = {'index':'TWENTY_INDEX', 'currency':'USD','timeFrame':'DAILY'}
 
 #resp = requests.post(stage, json=data)
 #resp = requests.post(local, json=data)
-# resp = requests.post(prod, json=data)
+resp = requests.post(prod, json=data)
 
-# print resp.text
+print resp.text
 
-
-for i in range(0,15):
-	print requests.get("http://coincap.io/global").text
