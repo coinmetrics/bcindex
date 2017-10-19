de3d0565b12e5e94fb7c3782a6b2882072dfcc47
steve fro <fro@uwalumni.com>
2017-10-19 10:45:56 -0500

    fix static values and bump index-data version for release

--- index-data/src/main/java/com/frobro/bcindex/web/constants/StaticValues.java
+++ index-data/src/main/java/com/frobro/bcindex/web/constants/StaticValues.java
@@ -5,15 +5,15 @@ package com.frobro.bcindex.web.constants;
  */
 public class StaticValues {
   // 10 index values
-  public static final double DIVISOR_TEN = 227007635.258235;
+  public static final double DIVISOR_TEN = 227027185.9;
   public static final double DIVISOR_EVEN_TEN = 122051563.779428;
   public static final double CONSTANT_TEN = 16627321.438;
   public static final double CONSTANT_EVEN_TEN = 2612479.09036185;
   public static final String MKT_CAP_FILE = "business_rules/oct_rebal.csv";
 
   // 20 index values
-  public static final double DIVISOR_20 = 12727725.7409682;
-  public static final double DIVISOR_EVEN_20 = 10912956.037548;
+  public static final double DIVISOR_20 = 11219050.63;
+  public static final double DIVISOR_EVEN_20 = 10407888.49;
   public static final String MKT_CAP_FILE_20 = "business_rules/20_oct_rebal.csv";
 
   // Ethereum index values
