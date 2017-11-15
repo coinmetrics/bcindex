0df38124f1c0ce1f0d9e5265ec0e5c6d50f23eba
Mike <Stoinkler@gmail.com>
2017-11-15 17:29:15 -0600

    november rebalance

--- index-data/src/main/java/com/frobro/bcindex/web/constants/StaticValues.java
+++ index-data/src/main/java/com/frobro/bcindex/web/constants/StaticValues.java
@@ -5,26 +5,26 @@ package com.frobro.bcindex.web.constants;
  */
 public class StaticValues {
   // 10 index values
-  public static final double DIVISOR_TEN = 227027185.9;
-  public static final double DIVISOR_EVEN_TEN = 122051563.779428;
-  public static final double CONSTANT_TEN = 16627321.438;
-  public static final double CONSTANT_EVEN_TEN = 2612479.09036185;
-  public static final String MKT_CAP_FILE = "business_rules/oct_rebal.csv";
+  public static final double DIVISOR_TEN = 228038808.3;
+  public static final double DIVISOR_EVEN_TEN = 112267430.7;
+  public static final double CONSTANT_TEN = 16679780.18;
+  public static final double CONSTANT_EVEN_TEN = 2651575.437;
+  public static final String MKT_CAP_FILE = "business_rules/nov_rebal.csv";
 
   // 20 index values
-  public static final double DIVISOR_20 = 11219050.63;
-  public static final double DIVISOR_EVEN_20 = 10407888.49;
-  public static final String MKT_CAP_FILE_20 = "business_rules/20_oct_rebal.csv";
+  public static final double DIVISOR_20 = 14569513.34;
+  public static final double DIVISOR_EVEN_20 = 13588064.55;
+  public static final String MKT_CAP_FILE_20 = "business_rules/20_nov_rebal.csv";
 
     // 40 index values
-  public static final double DIVISOR_40 = 43869638.39;
-  public static final double DIVISOR_EVEN_40 = 43869638.39;
-  public static final String MKT_CAP_FILE_40 = "business_rules/40_oct_rebal.csv";
+  public static final double DIVISOR_40 = 46520012.05;
+  public static final double DIVISOR_EVEN_40 = 44822700.27;
+  public static final String MKT_CAP_FILE_40 = "business_rules/40_nov_rebal.csv";
 
   // Ethereum index values
-  public static final double DIVISOR_ETHER = 70706194.7234075;
-  public static final double DIVISOR_EVEN_ETHER = 60413786.1399758;
-  public static final String MKT_CAP_FILE_EHTER = "business_rules/eth_oct.csv";
+  public static final double DIVISOR_ETHER = 66521043.32;
+  public static final double DIVISOR_EVEN_ETHER = 59614852.49;
+  public static final String MKT_CAP_FILE_EHTER = "business_rules/eth_nov.csv";
 
     // Application sector index values
   public static final double DIVISOR_APP = 56029891.92909;
