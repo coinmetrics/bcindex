91df86c994616dc31462e6a6aae379efd22b8a2a
steve fro <fro@uwalumni.com>
2017-11-22 20:47:28 -0600

    update test script

--- utils/apitest.py
+++ utils/apitest.py
@@ -2,15 +2,15 @@ import requests
 import json
 import datetime
 
-local = "http://localhost:8090/api/index"
+local = "http://localhost:8090/blet/index"
 stage = "http://stage-index-view.herokuapp.com/api/index"
 prod = "http://www.bletchleyindexes.com/api/index";
 
-data = {'index':'TWENTY_INDEX', 'currency':'USD','timeFrame':'DAILY'}
+data = {'index':'ODD_INDEX', 'currency':'USD','timeFrame':'DAILY'}
 
+resp = requests.post(local, json=data)
 #resp = requests.post(stage, json=data)
-#resp = requests.post(local, json=data)
-resp = requests.post(prod, json=data)
+#resp = requests.post(prod, json=data)
 
 print resp.text
 
