e8ad585087f6bbcbad70d1193fd01df427e9ec88
Muhammad Khan <mk_muhammad.khan@hotmail.com>
2018-02-01 21:28:25 -0600

    got the main functionaility working, graph isn't going all the way   :thinking:

--- index-view/src/main/resources/static/js/GraphService.js
+++ index-view/src/main/resources/static/js/GraphService.js
@@ -1,5 +1,6 @@
 import config from './config.js';
 import StockInfoService from './StockInfoService.js';
+import color from 'color'
 
 export default class GraphService {
     constructor(ctxElem, chartConfig={}) {        
@@ -9,21 +10,21 @@ export default class GraphService {
     }
     
     init() {
-        fetch(config.data)
+        fetch(config.mult)
         .then(r => r.text())
         .then(d => JSON.parse(d))
         .then((result) => {         
             debugger;            
             let formatter = config.formatter[result.currency] ? 
                     config.formatter[result.currency] : 
-                    config.formatter.default;    
+                    config.formatter.default;
             formatter.tickerInfoLastPrice(result);
             this.options.scales.xAxes[0].time.unit=result.timeUnit;
-            this.renderLineChart(result.times, result.data);
+            let graph = this.transformDataToChartJsDataSet(result);
+            this.renderLineChart(graph.labels, graph.dataset);
             this.stockInfoService.draw(result);
         });
     }
-
     update(payload) {
         fetch(config.data, {
             headers: {
@@ -41,27 +42,48 @@ export default class GraphService {
             formatter.tickerInfoLastPrice(result);
             this.options.scales.xAxes[0].time.unit=result.timeUnit;
             this.chart.destroy();
-            this.renderLineChart(result.times, result.data);
+            let graph = this.transformDataToChartJsDataSet(result.data);
+            this.renderLineChart(graph.labels, graph.dataset);
             this.stockInfoService.draw(result);
         });
     }
 
+    transformDataToChartJsDataSet(model) {
+        let result = new Array();
+        var lineColor = color('rgb(104, 140, 33)');
+        let object = (data, lineColor) => {
+            return {
+                label: 'Price',
+                data: data,
+                /*backgroundColor: config.colors.shadeRGBColor('rgb(104, 140, 33)', 5),*/
+                borderColor: lineColor.lighten(0.5),
+                borderWidth: 2,
+                pointRadius: 0,
+                pointHoverRadius: 4,
+                pointHoverBorderWidth: 4,
+                pointBackgroundColor: lineColor.lighten(0.5),
+            }
+        }
+        for (let data of model.prices) {
+            result.push(object(data, lineColor));
+        }        
+        model.times = [].concat.apply([], model.times).sort(function(a,b){
+            var c = new Date(a);
+            var d = new Date(b);
+            return c-d;
+        });
+        return {
+            dataset: result,
+            labels: model.times
+        };
+    }
+    
     renderLineChart(labels, data) {
         this.chart = new Chart(this.ctx, {
             type: 'line',
             data: {
                 labels: labels,
-                datasets: [{
-                    label: 'Price',
-                    data: data,
-                    backgroundColor: 'rgba(104, 140, 33, 0.0)' ,
-                    borderColor: 'rgba(104, 140, 33, 1)',
-                    borderWidth: 2,
-                    pointRadius: 0,
-                    pointHoverRadius: 4,
-                    pointHoverBorderWidth: 4,
-                    pointBackgroundColor: 'rgba(104, 140, 33, 1)',
-                }]
+                datasets: data
             },
             options: this.options
         });
