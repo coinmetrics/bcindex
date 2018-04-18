46b945574cb0a04185e60be9c36c984c2b875f93
Mike <Stoinkler@gmail.com>
2018-04-18 16:37:23 -0500

    april rebalance

--- index-data/src/main/java/com/frobro/bcindex/web/constants/StaticValues.java
+++ index-data/src/main/java/com/frobro/bcindex/web/constants/StaticValues.java
@@ -6,48 +6,48 @@ package com.frobro.bcindex.web.constants;
 public class StaticValues {
   // 10 index values
   public static final String TEN_INDEX_NAME = "10 INDEX";
-  public static final double DIVISOR_TEN = 240639512.6d;
-  public static final double DIVISOR_EVEN_TEN = 79279311.23;
+  public static final double DIVISOR_TEN = 242529911.8;
+  public static final double DIVISOR_EVEN_TEN = 72674006.86;
   public static final double CONSTANT_TEN = 16922812;
   public static final double CONSTANT_EVEN_TEN = 3122343.809;
-  public static final String MKT_CAP_FILE = "business_rules/10_march_rebal.csv";
+  public static final String MKT_CAP_FILE = "business_rules/10_april_rebal.csv";
 
   // 20 index values
   public static final String TWENTY_INDEX_NAME = "20 INDEX";
-  public static final double DIVISOR_20 = 17725935.38;
-  public static final double DIVISOR_EVEN_20 = 16981593.49;
-  public static final String MKT_CAP_FILE_20 = "business_rules/20_march_rebal.csv";
+  public static final double DIVISOR_20 = 18497975.94;
+  public static final double DIVISOR_EVEN_20 = 17341226.91;
+  public static final String MKT_CAP_FILE_20 = "business_rules/20_april_rebal.csv";
 
     // 40 index values
     public static final String FORTY_INDEX_NAME = "40 INDEX";
-  public static final double DIVISOR_40 = 51236411.4;
-  public static final double DIVISOR_EVEN_40 = 42411086.19;
-  public static final String MKT_CAP_FILE_40 = "business_rules/40_march_rebal.csv";
+  public static final double DIVISOR_40 = 51471319.22;
+  public static final double DIVISOR_EVEN_40 = 42775612.78;
+  public static final String MKT_CAP_FILE_40 = "business_rules/40_april_rebal.csv";
 
   // Ethereum index values
   public static final String ETH_INDEX_NAME = "Ethereum INDEX";
-  public static final double DIVISOR_ETHER = 123579733.2;
-  public static final double DIVISOR_EVEN_ETHER = 111114231.1;
-  public static final String MKT_CAP_FILE_EHTER = "business_rules/ETH_march_rebal.csv";
+  public static final double DIVISOR_ETHER = 87836126.84;
+  public static final double DIVISOR_EVEN_ETHER = 78783363.17;
+  public static final String MKT_CAP_FILE_EHTER = "business_rules/ETH_april_rebal.csv";
 
     // Total index values
   public static final String TOTAL_INDEX_NAME = "Total INDEX";
-  public static final double DIVISOR_TOTAL = 628098096.8;
-  public static final double DIVISOR_EVEN_TOTAL = 613495487.3;
-  public static final String MKT_CAP_FILE_TOTAL = "business_rules/Total_march_rebal.csv";
+  public static final double DIVISOR_TOTAL = 636310016.3;
+  public static final double DIVISOR_EVEN_TOTAL = 526827601;
+  public static final String MKT_CAP_FILE_TOTAL = "business_rules/Total_april_rebal.csv";
 
       // Currency index values
   public static final String CURR_INDEX_NAME = "currency index";
-  public static final double DIVISOR_CURRENCY = 2990050253d;
-  public static final String MKT_CAP_FILE_CURRENCY = "business_rules/Currency_march_rebal.csv";
+  public static final double DIVISOR_CURRENCY = 2879096430;
+  public static final String MKT_CAP_FILE_CURRENCY = "business_rules/Currency_april_rebal.csv";
 
         // Platform index values
   public static final String PLATFORM_INDEX_NAME = "platform index";
-  public static final double DIVISOR_PLATFORM = 2128586133;
-  public static final String MKT_CAP_FILE_PLATFORM = "business_rules/Platform_march_rebal.csv";
+  public static final double DIVISOR_PLATFORM = 2096419301;
+  public static final String MKT_CAP_FILE_PLATFORM = "business_rules/Platform_april_rebal.csv";
 
         // Application index values
   public static final String APP_INDEX_NAME = "application index";
-  public static final double DIVISOR_APPLICATION = 237340443.9;
-  public static final String MKT_CAP_FILE_APPLICATION = "business_rules/Application_march_rebal.csv";
+  public static final double DIVISOR_APPLICATION = 231719280.1;
+  public static final String MKT_CAP_FILE_APPLICATION = "business_rules/Application_april_rebal.csv";
 }
