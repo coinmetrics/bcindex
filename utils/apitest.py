0a0144af457c85fe9d5adf086907a2d133df8c5b
steve fro <sfro@uwalumni.com>
2018-05-20 21:30:40 -0500

    allow easy switching of env in apitest

--- utils/apitest.py
+++ utils/apitest.py
@@ -2,43 +2,52 @@ import requests
 import json
 import time
 
-def checkIndex():
-    local = "http://localhost:8090/api/index"
-    stage = "http://stage-index-view.herokuapp.com/api/index"
-    prod = "http://www.bletchleyindexes.com/api/index";
+local = "http://localhost:8090"
+stage = "http://stage-index-view.herokuapp.com"
+prod = "http://www.bletchleyindexes.com"
 
+ENV = stage
+
+def checkIndex():
+    endpt = ENV + "/api/index"
     data = {'index':'FORTY_INDEX', 'currency':'USD','timeFrame':'DAILY'}
 
-    return requests.post(local, json=data)
+    return requests.post(endpt, json=data)
     #resp = requests.post(stage, json=data)
     #resp = requests.post(prod, json=data)
 
 def checkWeights():
-    local = "http://localhost:8090/api/weight"
-    stage = "http://stage-index-view.herokuapp.com/api/weight"
-    prod = "http://www.bletchleyindexes.com/api/weight";
-
+    endpt = ENV + "/api/weight"
     data = {'index':'TEN'}
-
-    return requests.post(stage, json=data)
+    return requests.post(endpt, json=data)
 
 def checkWeightList():
-    local = "http://localhost:8090/api/weight/list"
-    stage = "http://stage-index-view.herokuapp.com/api/weight/list"
-    prod = "http://www.bletchleyindexes.com/api/weight/list";
-
+    endpt = ENV + "/api/weight/list"
     # data = {'indexList':['TEN','TWENTY','FORTY']}
     data = {'indexList':['TEN','TEN_EVEN','TWENTY','TWENTY_EVEN','FORTY','FORTY_EVEN','TOTAL','TOTAL_EVEN','ETHEREUM','ETHEREUM_EVEN','CURRENCY','PLATFORM','APPLICATION']}
 
-    return requests.post(prod, json=data)
+    return requests.post(endpt, json=data)
+
+def checkPrice():
+    endpt = ENV + "/blet/index";
+    data = {'index':'FORTY_INDEX','currency':'USD','timeFrame':'MAX'}
+
+    return requests.post(endpt, json=data)
+
+def checkDailyPrice():
+    endpt = ENV + "/daily/ten"
+    return requests.get(endpt)
+
+### api ###
+localapi = "http://localhost:8085"
+stageapi = "http://stage-index-api.herokuapp.com"
+prodapi = "http://bc-index-api.herokuapp.com"
+APIENV = stageapi
 
 def checkDailyWeight():
-    local = "http://localhost:8085/daily/weight"
-    prod = "????/daily/weight";
-    stage = "https://stage-index-api.herokuapp.com/daily/weight"
-
+    endpt = APIENV + "/daily/weight"
     data = {'index':'TEN'}
-    return requests.post(stage, json=data)
+    return requests.post(endpt, json=data)
 
 def checkDailyWeightContents():
     for entry in checkDailyWeight().json()['elementList']:
@@ -48,23 +57,11 @@ def toLocalDate(epoch):
     seconds = epoch/1000
     return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(seconds))
 
-def checkPrice():
-    prod = "http://www.bletchleyindexes.com/blet/index";
-    stage = "http://stage-index-view.herokuapp.com/blet/index"
-    local = "http://localhost:8090/blet/index"
-
-    data = {'index':'FORTY_INDEX','currency':'USD','timeFrame':'MAX'}
-
-    return requests.post(prod, json=data)
-
-def checkDailyPrice():
-    stage = "http://stage-index-view.herokuapp.com/daily/ten"
-    return requests.get(stage)
 
 # print checkPrice().text
 # print checkDailyPrice().text
 # print checkWeightList().text
-#print checkWeights().text
+# print checkWeights().text
 # print checkDailyWeight().text
 print checkDailyWeightContents()
 
