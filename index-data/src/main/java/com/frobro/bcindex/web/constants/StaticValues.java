087ed98f9f6dc6c9fb85fd2b674173c11b6eaf35
Mike Bletchley <mike@coinmetrics.io>
2020-07-31 12:32:04 -0500

    august rebal

--- index-data/src/main/java/com/frobro/bcindex/web/constants/StaticValues.java
+++ index-data/src/main/java/com/frobro/bcindex/web/constants/StaticValues.java
@@ -6,48 +6,48 @@ package com.frobro.bcindex.web.constants;
 public class StaticValues {
   // 10 index values
   public static final String TEN_INDEX_NAME = "10 INDEX";
-  public static final double DIVISOR_TEN = 337429466.79072994;
-  public static final double DIVISOR_EVEN_TEN = 184988468.7524982;
+  public static final double DIVISOR_TEN = 337037629.94939846;
+  public static final double DIVISOR_EVEN_TEN = 184490327.68129635;
   public static final double CONSTANT_TEN = 17064550;
   public static final double CONSTANT_EVEN_TEN = 17064550;
   public static final String MKT_CAP_FILE = "business_rules/10_august_rebal.csv";
 
   // 20 index values
   public static final String TWENTY_INDEX_NAME = "20 INDEX";
-  public static final double DIVISOR_20 = 34303982.17353767;
-  public static final double DIVISOR_EVEN_20 = 38248437.48087195;
+  public static final double DIVISOR_20 = 34699813.15207928;
+  public static final double DIVISOR_EVEN_20 = 38718055.438647084;
   public static final String MKT_CAP_FILE_20 = "business_rules/20_august_rebal.csv";
 
   // 40 index values
   public static final String FORTY_INDEX_NAME = "40 INDEX";
-  public static final double DIVISOR_40 = 93774105.7431215;
-  public static final double DIVISOR_EVEN_40 = 70473511.36279204;
+  public static final double DIVISOR_40 = 94143431.89608969;
+  public static final double DIVISOR_EVEN_40 = 70961758.34331104;
   public static final String MKT_CAP_FILE_40 = "business_rules/40_august_rebal.csv";
 
   // Ethereum index values
   public static final String ETH_INDEX_NAME = "Ethereum INDEX";
-  public static final double DIVISOR_ETHER = 134127990.4768138;
-  public static final double DIVISOR_EVEN_ETHER = 213643719.28156164;
+  public static final double DIVISOR_ETHER = 134203062.23077723;
+  public static final double DIVISOR_EVEN_ETHER = 218589005.2657651;
   public static final String MKT_CAP_FILE_ETHER = "business_rules/ETH_august_rebal.csv";
 
     // Total index values
   public static final String TOTAL_INDEX_NAME = "Total INDEX";
-  public static final double DIVISOR_TOTAL = 897078952.8878881;
-  public static final double DIVISOR_EVEN_TOTAL = 1902076377.158795;
+  public static final double DIVISOR_TOTAL = 896829636.5671308;
+  public static final double DIVISOR_EVEN_TOTAL = 1945667107.777857;
   public static final String MKT_CAP_FILE_TOTAL = "business_rules/Total_august_rebal.csv";
 
       // Currency index values
   public static final String CURR_INDEX_NAME = "currency index";
-  public static final double DIVISOR_CURRENCY = 4309233776.363292;
+  public static final double DIVISOR_CURRENCY = 4309233776.363291;
   public static final String MKT_CAP_FILE_CURRENCY = "business_rules/Currency_august_rebal.csv";
 
         // Platform index values
   public static final String PLATFORM_INDEX_NAME = "platform index";
-  public static final double DIVISOR_PLATFORM = 4148945929.287739;
+  public static final double DIVISOR_PLATFORM = 4191913439.544366;
   public static final String MKT_CAP_FILE_PLATFORM = "business_rules/Platform_august_rebal.csv";
 
         // Application index values
   public static final String APP_INDEX_NAME = "application index";
-  public static final double DIVISOR_APPLICATION = 444393154.60173434;
+  public static final double DIVISOR_APPLICATION = 446903620.6923931;
   public static final String MKT_CAP_FILE_APPLICATION = "business_rules/Application_august_rebal.csv";
 }
